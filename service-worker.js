4bfe1257b04b3dabf69917e88858eefccf685db4
Micah Nacht <mjnacht@gmail.com>
2019-07-09 16:38:53 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/urban-carnivore-spotter/precache-manifest.179afdde42c970266860fd4535a389d1.js"
+  "/urban-carnivore-spotter/precache-manifest.fd30d7807f8d2ddf04cf845ac501d8da.js"
 );
 
 workbox.clientsClaim();
